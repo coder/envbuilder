300cedeae175a6ec715d4c0b8cc5c06d0e9c4338
Kyle Carberry <kyle@carberry.com>
2023-03-31 10:31:29 -0500

    Add CI (#2)

---
+++ scripts/version.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+cd $(dirname "${BASH_SOURCE[0]}")
+
+last_tag="$(git describe --tags --abbrev=0)"
+version="$last_tag"
+
+# Remove the "v" prefix.
+echo "${version#v}"
