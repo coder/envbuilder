e453999b445bdb5560fcaf4d8ff82be73d2d115e
Kyle Carberry <kyle@carberry.com>
2023-10-02 15:29:43 -0500

    chore: build for multiple architectures on release
    Fixes #52.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,33 @@
 #!/usr/bin/env bash
 
 cd $(dirname "${BASH_SOURCE[0]}")
-set -euxo pipefail
+set -euo pipefail
 
-CGO_ENABLED=0 go build -o ./envbuilder ../cmd/envbuilder
-docker build -t envbuilder:latest .
+archs=()
+for arg in "$@"; do
+  if [[ $arg == --arch=* ]]; then
+    arch="${arg#*=}"
+    archs+=( "$arch" )
+  else
+    echo "Unknown argument: $arg"
+    exit 1
+  fi
+done
+
+current=$(go env GOARCH)
+if [ ${#archs[@]} -eq 0 ]; then
+  echo "No architectures specified. Defaulting to $current..."
+  archs=( "$current" ) 
+fi
+
+for arch in "${archs[@]}"; do
+  GOARCH=$arch CGO_ENABLED=0 go build -o ./envbuilder-$arch ../cmd/envbuilder && \
+    docker build --build-arg PLATFORM=$arch -t envbuilder:${arch} -f Dockerfile . &
+done
+wait
+
+# Check if archs contains the current. If so, then output a message!
+if [[ " ${archs[@]} " =~ " ${current} " ]]; then
+  docker tag envbuilder:${arch} envbuilder:latest
+  echo "Tagged $current as envbuilder:latest!"
+fi
