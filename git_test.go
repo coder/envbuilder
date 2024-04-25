f828b1773ca84a90f47a1244f22e396221b84c04
Cian Johnston <cian@coder.com>
2024-04-25 10:51:47 +0000

    chore: refactor: add helpers to create commits

--- git_test.go
+++ git_test.go
@@ -9,14 +9,11 @@ import (
 	"os"
 	"regexp"
 	"testing"
-	"time"
 
 	"github.com/coder/envbuilder"
 	"github.com/coder/envbuilder/testutil/gittest"
 	"github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-billy/v5/memfs"
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing/object"
 	githttp "github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/stretchr/testify/require"
 )
@@ -77,13 +74,16 @@ func TestCloneRepo(t *testing.T) {
 
 			// We do not overwrite a repo if one is already present.
 			t.Run("AlreadyCloned", func(t *testing.T) {
-				srvURL := setupGit(t, tc.srvUsername, tc.srvPassword)
+				srvFS := memfs.New()
+				_ = gittest.NewRepo(t, srvFS, gittest.Commit(t, "README.md", "Hello, world!", "Wow!"))
+				authMW := gittest.BasicAuthMW(tc.srvUsername, tc.srvPassword)
+				srv := httptest.NewServer(authMW(gittest.NewServer(srvFS)))
 				clientFS := memfs.New()
 				// A repo already exists!
 				_ = gittest.NewRepo(t, clientFS)
 				cloned, err := envbuilder.CloneRepo(context.Background(), envbuilder.CloneRepoOptions{
 					Path:    "/",
-					RepoURL: srvURL,
+					RepoURL: srv.URL,
 					Storage: clientFS,
 				})
 				require.NoError(t, err)
@@ -93,12 +93,15 @@ func TestCloneRepo(t *testing.T) {
 			// Basic Auth
 			t.Run("BasicAuth", func(t *testing.T) {
 				t.Parallel()
-				srvURL := setupGit(t, tc.srvUsername, tc.srvPassword)
+				srvFS := memfs.New()
+				_ = gittest.NewRepo(t, srvFS, gittest.Commit(t, "README.md", "Hello, world!", "Wow!"))
+				authMW := gittest.BasicAuthMW(tc.srvUsername, tc.srvPassword)
+				srv := httptest.NewServer(authMW(gittest.NewServer(srvFS)))
 				clientFS := memfs.New()
 
 				cloned, err := envbuilder.CloneRepo(context.Background(), envbuilder.CloneRepoOptions{
 					Path:    "/workspace",
-					RepoURL: srvURL,
+					RepoURL: srv.URL,
 					Storage: clientFS,
 					RepoAuth: &githttp.BasicAuth{
 						Username: tc.username,
@@ -117,14 +120,18 @@ func TestCloneRepo(t *testing.T) {
 				require.Equal(t, "Hello, world!", readme)
 				gitConfig := mustRead(t, clientFS, "/workspace/.git/config")
 				// Ensure we do not modify the git URL that folks pass in.
-				require.Regexp(t, fmt.Sprintf(`(?m)^\s+url\s+=\s+%s\s*$`, regexp.QuoteMeta(srvURL)), gitConfig)
+				require.Regexp(t, fmt.Sprintf(`(?m)^\s+url\s+=\s+%s\s*$`, regexp.QuoteMeta(srv.URL)), gitConfig)
 			})
 
 			// In-URL-style auth e.g. http://user:password@host:port
 			t.Run("InURL", func(t *testing.T) {
 				t.Parallel()
-				srvURL := setupGit(t, tc.srvUsername, tc.srvPassword)
-				authURL, err := url.Parse(srvURL)
+				srvFS := memfs.New()
+				_ = gittest.NewRepo(t, srvFS, gittest.Commit(t, "README.md", "Hello, world!", "Wow!"))
+				authMW := gittest.BasicAuthMW(tc.srvUsername, tc.srvPassword)
+				srv := httptest.NewServer(authMW(gittest.NewServer(srvFS)))
+
+				authURL, err := url.Parse(srv.URL)
 				require.NoError(t, err)
 				authURL.User = url.UserPassword(tc.username, tc.password)
 				clientFS := memfs.New()
@@ -159,29 +166,4 @@ func mustRead(t *testing.T, fs billy.Filesystem, path string) string {
 	content, err := io.ReadAll(f)
 	require.NoError(t, err)
 	return string(content)
-}
-
-func setupGit(t *testing.T, user, pass string) (url string) {
-	serverFS := memfs.New()
-	repo := gittest.NewRepo(t, serverFS)
-	tree, err := repo.Worktree()
-	require.NoError(t, err)
-
-	gittest.WriteFile(t, serverFS, "README.md", "Hello, world!")
-	_, err = tree.Add("README.md")
-	require.NoError(t, err)
-	commit, err := tree.Commit("Wow!", &git.CommitOptions{
-		Author: &object.Signature{
-			Name:  "Example",
-			Email: "in@tests.com",
-			When:  time.Now(),
-		},
-	})
-	require.NoError(t, err)
-	_, err = repo.CommitObject(commit)
-	require.NoError(t, err)
-
-	authMW := gittest.BasicAuthMW(user, pass)
-	srv := httptest.NewServer(authMW(gittest.NewServer(serverFS)))
-	return srv.URL
 }
