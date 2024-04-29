deab071eeab1939535672563f3a77a0c239da609
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2024-04-29 14:16:22 -0300

    Apply suggestions from Danny's code review
    Co-authored-by: Danny Kopping <dannykopping@gmail.com>

--- options_test.go
+++ options_test.go
@@ -15,14 +15,15 @@ type testEnvCase struct {
 	Actual   func() interface{}
 }
 
-func TestEnvOptions(t *testing.T) {
+// TestEnvOptionParsing tests that given environment variables of different types are handled as expected.
+func TestEnvOptionParsing(t *testing.T) {
 	t.Setenv("SETUP_SCRIPT", "setup.sh")
 	t.Setenv("CACHE_TTL_DAYS", "7")
 	t.Setenv("IGNORE_PATHS", "/var,/tmp")
 	t.Setenv("SKIP_REBUILD", "true")
 	t.Setenv("GIT_CLONE_SINGLE_BRANCH", "")
 
-	var o = envbuilder.Options{}
+	var o envbuilder.Options
 	cmd := serpent.Command{
 		Options: o.CLI(),
 		Handler: func(inv *serpent.Invocation) error {
