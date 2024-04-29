73696aee3cbb63ed1b47e8cad3be78a2a5edf3bb
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2024-04-29 15:18:24 +0000

    test: add test for options env

---
+++ options_test.go
@@ -0,0 +1,40 @@
+package envbuilder_test
+
+import (
+	"testing"
+
+	"github.com/coder/envbuilder"
+	"github.com/coder/serpent"
+	"github.com/stretchr/testify/require"
+)
+
+type testEnvCase struct {
+	Env      string
+	EnvValue string
+	Expected interface{}
+	Actual   func() interface{}
+}
+
+func TestEnvOptions(t *testing.T) {
+	t.Setenv("SETUP_SCRIPT", "setup.sh")
+	t.Setenv("CACHE_TTL_DAYS", "7")
+	t.Setenv("IGNORE_PATHS", "/var,/tmp")
+	t.Setenv("SKIP_REBUILD", "true")
+	t.Setenv("GIT_CLONE_SINGLE_BRANCH", "")
+
+	var o = envbuilder.Options{}
+	cmd := serpent.Command{
+		Options: o.CLI(),
+		Handler: func(inv *serpent.Invocation) error {
+			return nil
+		},
+	}
+	err := cmd.Invoke().WithOS().Run()
+	require.NoError(t, err)
+
+	require.Equal(t, o.SetupScript, "setup.sh")
+	require.Equal(t, o.CacheTTLDays, int64(7))
+	require.Equal(t, o.IgnorePaths, []string{"/var", "/tmp"})
+	require.True(t, o.SkipRebuild)
+	require.False(t, o.GitCloneSingleBranch)
+}
