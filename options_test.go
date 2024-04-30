3a7e2e5b8028dacbc65342500cf154d1606670b0
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2024-04-30 13:57:57 +0000

    Improve readability

--- options_test.go
+++ options_test.go
@@ -10,68 +10,77 @@ import (
 )
 
 // TestEnvOptionParsing tests that given environment variables of different types are handled as expected.
-func TestTestEnvOptionParsing(t *testing.T) {
+func TestEnvOptionParsing(t *testing.T) {
 	t.Run("string", func(t *testing.T) {
-	        const val = "setup.sh"
+		const val = "setup.sh"
 		t.Setenv("SETUP_SCRIPT", val)
-
-		var o envbuilder.Options
-		err := runCLI(&o)
-		require.NoError(t, err)
-
+		o := runCLI()
 		require.Equal(t, o.SetupScript, val)
 	})
 
 	t.Run("int", func(t *testing.T) {
 		t.Setenv("CACHE_TTL_DAYS", "7")
-
-		var o envbuilder.Options
-		err := runCLI(&o)
-		require.NoError(t, err)
-
+		o := runCLI()
 		require.Equal(t, o.CacheTTLDays, int64(7))
 	})
 
 	t.Run("string array", func(t *testing.T) {
 		t.Setenv("IGNORE_PATHS", "/var,/temp")
-
-		var o envbuilder.Options
-		err := runCLI(&o)
-		require.NoError(t, err)
-
+		o := runCLI()
 		require.Equal(t, o.IgnorePaths, []string{"/var", "/temp"})
 	})
 
 	t.Run("bool", func(t *testing.T) {
-		t.Setenv("SKIP_REBUILD", "true")
-		t.Setenv("GIT_CLONE_SINGLE_BRANCH", "false")
-		t.Setenv("EXIT_ON_BUILD_FAILURE", "true")
-		t.Setenv("FORCE_SAFE", "false")
-		t.Setenv("INSECURE", "true")
+		t.Run("lowercase", func(t *testing.T) {
+			t.Setenv("SKIP_REBUILD", "true")
+			t.Setenv("GIT_CLONE_SINGLE_BRANCH", "false")
+			o := runCLI()
+			require.True(t, o.SkipRebuild)
+			require.False(t, o.GitCloneSingleBranch)
+		})
 
-		var o envbuilder.Options
-		err := runCLI(&o)
-		require.NoError(t, err)
+		t.Run("uppercase", func(t *testing.T) {
+			t.Setenv("SKIP_REBUILD", "TRUE")
+			t.Setenv("GIT_CLONE_SINGLE_BRANCH", "FALSE")
+			o := runCLI()
+			require.True(t, o.SkipRebuild)
+			require.False(t, o.GitCloneSingleBranch)
+		})
 
-		require.True(t, o.SkipRebuild)
-		require.False(t, o.GitCloneSingleBranch)
-		require.True(t, o.ExitOnBuildFailure)
-		require.False(t, o.ForceSafe)
-		require.True(t, o.Insecure)
+		t.Run("numeric", func(t *testing.T) {
+			t.Setenv("SKIP_REBUILD", "1")
+			t.Setenv("GIT_CLONE_SINGLE_BRANCH", "0")
+			o := runCLI()
+			require.True(t, o.SkipRebuild)
+			require.False(t, o.GitCloneSingleBranch)
+		})
+
+		t.Run("empty", func(t *testing.T) {
+			t.Setenv("GIT_CLONE_SINGLE_BRANCH", "")
+			o := runCLI()
+			require.False(t, o.GitCloneSingleBranch)
+		})
 	})
 }
 
-func runCLI(o *envbuilder.Options) error {
+func runCLI() envbuilder.Options {
+	var o envbuilder.Options
 	cmd := serpent.Command{
 		Options: o.CLI(),
 		Handler: func(inv *serpent.Invocation) error {
 			return nil
 		},
 	}
+
 	i := cmd.Invoke().WithOS()
 	fakeIO(i)
 	err := i.Run()
-	return err
+
+	if err != nil {
+		panic("failed to run CLI: " + err.Error())
+	}
+
+	return o
 }
 
 type ioBufs struct {
