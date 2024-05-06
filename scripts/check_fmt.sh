79fd1c74cfb1781ee98cad95a3e5ff87aaf270c0
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2024-05-06 13:43:48 +0000

    chore: improve fmt scripts

--- scripts/check_fmt.sh
+++ scripts/check_fmt.sh
@@ -2,7 +2,7 @@
 
 list="$(go run mvdan.cc/gofumpt@v0.6.0 -l .)"
 if [[ -n $list ]]; then
-  echo -e "error: The following files have changes:\n\n${list}\n\nDiff:\n\n"
+  echo -n -e "error: The following files have changes:\n\n${list}\n\nDiff:\n\n"
   go run mvdan.cc/gofumpt@v0.6.0 -d .
   exit 1
 fi
