7d895ef84452316a2a142abdf18c688d2c56411d
Cian Johnston <cian@coder.com>
2024-09-25 16:48:05 +0100

    chore(scripts/diagram.sh): export in PNG format as well

--- scripts/diagram.sh
+++ scripts/diagram.sh
@@ -3,5 +3,8 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 set -euxo pipefail
 
-d2 ./diagram.d2 --pad=32 -t 1 ./diagram-light.svg
-d2 ./diagram.d2 --pad=32 -t 200 ./diagram-dark.svg
+formats=( svg png )
+for format in "${formats[@]}"; do
+  d2 ./diagram.d2 --pad=32 -t 1 "./diagram-light.${format}"
+  d2 ./diagram.d2 --pad=32 -t 200 "./diagram-dark.${format}"
+done
