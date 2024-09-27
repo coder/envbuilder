d0bf8c4b6fcd234157bb19eae88b965e7b291d4b
Cian Johnston <cian@coder.com>
2024-09-27 14:28:40 +0100

    add preExec to Run and RunCacheProbe

--- cmd/envbuilder/main.go
+++ cmd/envbuilder/main.go
@@ -37,6 +37,7 @@ func envbuilderCmd() serpent.Command {
 		Options: o.CLI(),
 		Handler: func(inv *serpent.Invocation) error {
 			o.SetDefaults()
+			preExec := make([]func(), 0)
 			o.Logger = log.New(os.Stderr, o.Verbose)
 			if o.CoderAgentURL != "" {
 				if o.CoderAgentToken == "" {
@@ -50,6 +51,10 @@ func envbuilderCmd() serpent.Command {
 				if err == nil {
 					o.Logger = log.Wrap(o.Logger, coderLog)
 					defer closeLogs()
+					preExec = append(preExec, func() {
+						o.Logger(log.LevelInfo, "Closing logs")
+						closeLogs()
+					})
 					// This adds the envbuilder subsystem.
 					// If telemetry is enabled in a Coder deployment,
 					// this will be reported and help us understand
@@ -78,7 +83,7 @@ func envbuilderCmd() serpent.Command {
 				return nil
 			}
 
-			err := envbuilder.Run(inv.Context(), o)
+			err := envbuilder.Run(inv.Context(), o, preExec...)
 			if err != nil {
 				o.Logger(log.LevelError, "error: %s", err)
 			}
